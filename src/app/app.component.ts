792ccb867dad7f0da28b00922f1f68bce810ba75
Anil Kumar <anilkumar.dcttechnology@gmail.com>
2025-06-14 15:56:49 +0530

    Conversion observable to signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,24 @@ import { interval } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
+  //observable using property
+  interval$ = interval(100000000);
+  intervalSignal = toSignal(this.interval$);
   ngOnInit(): void {
     //below is observable that generates values every 1 second
-    const source = interval(1000).subscribe(() => console.log('Tick'));
+    // const source = interval(1000)
+    //   .pipe(map((x) => x + 1))
+    //   .subscribe(() => console.log('Tick')); //adding map operator to get the understanding that we can use operator with observables
+    // this.destroyRef.onDestroy(() => {
+    //   source.unsubscribe();
+    // });
+    //Use case of observable
+    const source = this.interval$
+      .pipe(map((x) => x + 1))
+      .subscribe(() => console.log('Tick')); //adding map operator to get the understanding that we can use operator with observables
     this.destroyRef.onDestroy(() => {
       source.unsubscribe();
     });
   }
 }
+//to convert signal to observable use toSignal and for observable to signal use toObservable
